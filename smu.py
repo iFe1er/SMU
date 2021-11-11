93c465e12975e0bfc02abe064f81e044ab9604cd
Fuhang <1341414814@qq.com>
2021-11-11 12:31:45 +0800

    Create smu.py
    init Tensorflow code

---
+++ smu.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+import tensorflow as tf
+
+def SMU(x,alpha=0.25):
+    mu = tf.compat.v1.get_variable('SMU_mu', x.get_shape()[-1],
+                       initializer=tf.constant_initializer(1000000),
+                       dtype=tf.float32)
+    return ((1+alpha)*x + (1-alpha)*x*tf.math.erf(mu*(1-alpha)*x))/2
+
+def SMU1(x,alpha=0.25):
+    mu = tf.compat.v1.get_variable('SMU1_mu', x.get_shape()[-1],
+                       initializer=tf.constant_initializer(4.352665993287951e-9),
+                       dtype=tf.float32)
+    return ((1+alpha)*x+tf.math.sqrt(tf.math.square(x-alpha*x)+tf.math.square(mu)))/2
+    
+def test_SMU(x):
+    print(SMU(x))
+    
+def test_SMU1(x):
+    print(SMU1(x))
+
+def test():
+    x = tf.convert_to_tensor(np.array([[-0.6],[0.6]]),dtype=tf.float32)
+    test_SMU(x)
+    test_SMU1(x)
+
+if __name__ == '__main__':
+    test()
