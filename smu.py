23e73558a0f0b045125e46f9584389b50945cefb
Fuhang <1341414814@qq.com>
2021-11-11 15:41:14 +0800

    Update smu.py

--- smu.py
+++ smu.py
@@ -3,13 +3,13 @@
 import tensorflow as tf
 
 def SMU(x,alpha=0.25):
-    mu = tf.compat.v1.get_variable('SMU_mu', x.get_shape()[-1],
+    mu = tf.compat.v1.get_variable('SMU_mu', shape=(),
                        initializer=tf.constant_initializer(1000000),
                        dtype=tf.float32)
     return ((1+alpha)*x + (1-alpha)*x*tf.math.erf(mu*(1-alpha)*x))/2
 
 def SMU1(x,alpha=0.25):
-    mu = tf.compat.v1.get_variable('SMU1_mu', x.get_shape()[-1],
+    mu = tf.compat.v1.get_variable('SMU1_mu', shape=(),
                        initializer=tf.constant_initializer(4.352665993287951e-9),
                        dtype=tf.float32)
     return ((1+alpha)*x+tf.math.sqrt(tf.math.square(x-alpha*x)+tf.math.square(mu)))/2
